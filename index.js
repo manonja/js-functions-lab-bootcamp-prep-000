8e454fd92e00e0f3605d916f6ec7fec9617e0ec7
manon jacquin <manon.jacquin@gmail.com>
2018-09-18 15:47:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name){
+  // return an expression including the argument value we passed in the function
   return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(holiday, name){
+  // return an expression with the two arguments values we passed in the function
   return `Happy ${holiday}, ${name}!`
 }
 
